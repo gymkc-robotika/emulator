d9338517167ebb68bcb41c200374647809439c4e
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-05-02 14:48:01 +0200

    Intial robot speed emulation.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
 
-class MBot {
+
+#define M1 0
+
+class MeDCMotor {
 public:
+    int motorNumber;
+    int speed;
+    explicit MeDCMotor(int number) {
+      motorNumber = number;
+      speed = 0;
+    }
+    void run(int s) {
+      speed = s;
+    }
+};
+
+
+class MBot {
+    MeDCMotor *motorR;
+    MeDCMotor *motorL;
+public:
+    MBot(MeDCMotor *rm, MeDCMotor *lm) {
+      motorR = rm;
+      motorL = lm;
+    }
     // coordinates
     double x = 0, y = 0;
+    double heading = 0;
     // current speed
     double speedX = 0, speedY = 0;
 
     void move(double dt) {
-      x += speedX * dt;
-      y += speedY * dt;
+      // TODO: inertia / acceleration
+      double speed = (motorR->speed + motorL->speed) * 0.001;
+      // TODO: heading
+
+      x += speed * dt;
+      y += 0;
     }
 };
+
 
 // declare imports - expect the mBot sketch
 void loop();
 void setup();
 
 int main() {
-  MBot bot = MBot();
+// forward declare motors
+  extern MeDCMotor motor_9;
+  extern MeDCMotor motor_10;
+  MBot bot = MBot(&motor_9, &motor_10);
   printf("Emulator started - coordinates %.3f,%.3f\n", bot.x, bot.y);
   double dt = 0.1;
   setup();
@@ -31,8 +63,12 @@ int main() {
   return 0;
 }
 
-void setup() {
+MeDCMotor motor_9(9);
+MeDCMotor motor_10(10);
 
+void setup() {
+  motor_9.run((9)==M1?-(50):(50));
+  motor_10.run((10)==M1?-(50):(50));
 }
 void loop() {
 
