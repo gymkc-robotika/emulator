152196f358ff595fb0a710a2ecd7cd75c8502311
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-05-02 14:14:54 +0200

    Emulator skeleton (loop, setup).

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+class MBot {
+public:
+    // coordinates
+    double x = 0, y = 0;
+    // current speed
+    double speedX = 0, speedY = 0;
+
+    void move(double dt) {
+      x += speedX * dt;
+      y += speedY * dt;
+    }
+};
+
+// declare imports - expect the mBot sketch
+void loop();
+void setup();
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  MBot bot = MBot();
+  printf("Emulator started - coordinates %.3f,%.3f\n", bot.x, bot.y);
+  double dt = 0.1;
+  setup();
+  printf("Setup done");
+  for (int i = 0; i < 1000; i++) {
+    loop();
+    bot.move(dt);
+  }
+  printf("Final bot coordinates %.3f,%.3f\n", bot.x, bot.y);
+  return 0;
+}
+
+void setup() {
+
+}
+void loop() {
+
 }
