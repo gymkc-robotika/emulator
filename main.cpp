4eb83002be1d584612a15019e2dd3f48bfd47190
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-05-02 21:30:34 +0200

    Add basic heading support.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,57 @@
 #include <iostream>
+#include <cmath>
 
 
 #define M1 0
 
 class MeDCMotor {
-public:
-    int motorNumber;
-    int speed;
-    explicit MeDCMotor(int number) {
-      motorNumber = number;
-      speed = 0;
-    }
-    void run(int s) {
-      speed = s;
-    }
+  public:
+  int motorNumber;
+  int speed;
+
+  explicit MeDCMotor(int number) {
+    motorNumber = number;
+    speed = 0;
+  }
+
+  void run(int s) {
+    speed = s;
+  }
 };
 
 
 class MBot {
-    MeDCMotor *motorR;
-    MeDCMotor *motorL;
-public:
-    MBot(MeDCMotor *rm, MeDCMotor *lm) {
-      motorR = rm;
-      motorL = lm;
-    }
-    // coordinates
-    double x = 0, y = 0;
-    double heading = 0;
-    // current speed
-    double speedX = 0, speedY = 0;
+  MeDCMotor *motorR;
+  MeDCMotor *motorL;
 
-    void move(double dt) {
-      // TODO: inertia / acceleration
-      double speed = (motorR->speed + motorL->speed) * 0.001;
-      // TODO: heading
+  public:
+  MBot(MeDCMotor *rm, MeDCMotor *lm) {
+    motorR = rm;
+    motorL = lm;
+  }
 
-      x += speed * dt;
-      y += 0;
-    }
+  // coordinates
+  double x = 0, y = 0;
+  double heading = 0;
+  // current speed
+  double speedX = 0, speedY = 0;
+
+  void move(double dt) {
+    // TODO: inertia / acceleration
+    double speed = (motorR->speed + motorL->speed) * 0.001;
+    double headingChange = (motorR->speed - motorL->speed) * 0.1;
+
+    heading += headingChange * dt;
+
+    x += speed * dt * sin(heading);
+    y += speed * dt * cos(heading);
+  }
 };
 
 
 // declare imports - expect the mBot sketch
 void loop();
+
 void setup();
 
 int main() {
@@ -54,10 +62,13 @@ int main() {
   printf("Emulator started - coordinates %.3f,%.3f\n", bot.x, bot.y);
   double dt = 0.1;
   setup();
-  printf("Setup done");
+  printf("Setup done\n");
   for (int i = 0; i < 1000; i++) {
     loop();
     bot.move(dt);
+    if ((i % 50)==0) {
+      printf("Position %.3f,%.3f\n", bot.x, bot.y);
+    }
   }
   printf("Final bot coordinates %.3f,%.3f\n", bot.x, bot.y);
   return 0;
@@ -67,9 +78,10 @@ MeDCMotor motor_9(9);
 MeDCMotor motor_10(10);
 
 void setup() {
-  motor_9.run((9)==M1?-(50):(50));
-  motor_10.run((10)==M1?-(50):(50));
+  motor_9.run((9) == M1 ? -(100) : (100));
+  motor_10.run((10) == M1 ? -(80) : (80));
 }
+
 void loop() {
 
 }
