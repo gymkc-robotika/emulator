0dfcc5ba42fdcb48e6291e14d6604628f300313f
Ondrej Spanel <OndrejSpanel@users.noreply.github.com>
2019-05-16 16:27:05 +0200

    More complex behaviour.

--- mbotProgram.cpp
+++ mbotProgram.cpp
@@ -10,15 +10,65 @@ MeDCMotor motor_10(10);
 MeRGBLed rgbled_7(7, 7==7?2:4);
 MeLineFollower linefollower_2(2);
 
-void setup() {
-  motor_9.run((9) == M1 ? -(100) : (100));
-  motor_10.run((10) == M1 ? -(80) : (80));
+void motorRL(int l, int r) {
+  motor_9.run((9) == M1 ? -(l) : (l));
+  motor_10.run((10) == M1 ? -(r) : (r));
 }
 
+
+long start;
+enum State {
+	Init,
+	Turn,
+	Reverse,
+	ReverseForAWhile
+};
+
+State state = Init;
+
+
+void doInit() {
+	start =  millis();
+	state = Init;
+	motorRL(100, 80);
+}
+
+void setup() {
+	doInit();
+}
+
+long reverseStart;
+
 void loop() {
-   int line = linefollower_2.readSensors();
-   if (line !=0) {
-      motor_9.run((9) == M1 ? -(-80) : (-80));
-      motor_10.run((10) == M1 ? -(-80) : (-80));
-   }
+	if (state == Init) {
+	   int line = linefollower_2.readSensors();
+	   if (line !=0) {
+			motorRL(-80, -80);
+			state = Reverse;
+	   }
+	   if (millis() > start + 10000) {
+			motorRL(120, 150);
+			state = Turn;
+		}
+	}
+	if (state == Turn) {
+	   int line = linefollower_2.readSensors();
+	   if (line !=0) {
+			motorRL(-80, -80);
+			state = Reverse;
+	   }
+	}
+	if (state == Reverse) {
+	   int line = linefollower_2.readSensors();
+	   if (line == 0) {
+	   	  state = ReverseForAWhile;
+	   	  reverseStart = millis();
+	   }
+	}
+	if (state == ReverseForAWhile) {
+		if (millis() > reverseStart + 2000)  {
+			doInit();
+		}
+	}
+	
 }
